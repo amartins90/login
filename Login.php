dfb0e6410da7b11b67aedbe25fa8c062186865db
Alexandre J. Martins <alexandre@ajmartins.com.br>
2019-11-24 21:42:51 -0300

    Initial release

---
+++ Login.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This class implements a login service with encrypted password. 
+ * 
+ * @author Alexandre J. Martins <alexandre@ajmartins.com.br> github.com/amartins90
+ *
+ */
+class Login
+{
+	private $username;
+	private $password;
+	private $logged = false;
+
+	public function __construct($username, $password)
+	{
+		$this->username = $username;
+		$this->password = $password;
+
+		$this->login();
+	}
+
+	private function login()
+	{
+		$credentials = parse_ini_file('auth.ini');
+
+		if ($this->username == $credentials['username'] && password_verify($this->password, $credentials['password'])) {
+			$this->logged = true;
+		}
+	}
+
+	public function check()
+	{
+		return $this->logged;
+	}
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 
+ * @author Alexandre J. Martins <alexandre@ajmartins.com.br> github.com/amartins90
+ *
+ */
+include 'Login.php';
+
+if ($_POST) {
+
+	$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+	$login = new Login($username, $password);
+
+	if ($login->check()) {
+		echo "You're logged!";
+	} else {
+		echo "Login failed";
+	}
+}
